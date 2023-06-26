0291b9dfca07b33fcf8fb5c4b5178a4b2426bbcf
DominatingShot <akshajvj2003@gmail.com>
2023-06-26 20:14:19 +0400

    2

--- LEET-CODE/104.MAXIMUM_DEPTH_OF_BINARY_TREE/MAXIMUM_DEPTH_OF_BINARY_TREE.java
+++ LEET-CODE/104.MAXIMUM_DEPTH_OF_BINARY_TREE/MAXIMUM_DEPTH_OF_BINARY_TREE.java
@@ -1,21 +1,28 @@
 class Solution {
+    // Method to calculate the maximum depth of a binary tree
     public int maxDepth(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
-        else{
+        // Check if the root is null, indicating an empty tree
+        if (root == null) {
+            return 0; // Return 0 as the depth of an empty tree is 0
+        } else {
+            // Call the helper method to calculate the depth of the tree
             return depth(root, 0);
         }
     }
-    public int depth(TreeNode root , int n){
-        if(root == null){
-            return n;
+    
+    // Helper method to recursively calculate the depth of the binary tree
+    public int depth(TreeNode root, int n) {
+        // Check if the root is null, indicating a leaf node
+        if (root == null) {
+            return n; // Return the current depth as it is a leaf node
         }
-        if(root.left == null && root.right == null){
-            return n;
-        }
-        else{
-            return Math.max(depth(root.left, n+1), depth(root.right, n+1));
+        
+        // Check if both the left and right children are null, indicating a leaf node
+        if (root.left == null && root.right == null) {
+            return n; // Return the current depth as it is a leaf node
+        } else {
+            // Calculate the maximum depth between the left and right subtree recursively
+            return Math.max(depth(root.left, n + 1), depth(root.right, n + 1));
         }
     }
 }
--- LEET-CODE/392.IS_SUBSEQUENCE/IS_SUBSEQUENCE.cpp
+++ LEET-CODE/392.IS_SUBSEQUENCE/IS_SUBSEQUENCE.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        map <char, int> m;
+        map<char, int> m; // Create a map to store character frequencies
+        
+        // Iterate through each character in string t and update their frequencies in the map
         for (int i = 0; i < t.size(); i++) {
             m[t[i]]++;
         }
+        
+        // Iterate through each character in string s and check if it is present in the map
+        // If a character is not present or its frequency becomes 0, return false
         for (int i = 0; i < s.size(); i++) {
             if (m[s[i]] == 0) {
                 return false;
             }
-            m[s[i]]--;
+            m[s[i]]--; // Decrement the frequency of the character in the map
         }
-        return true;
+        
+        return true; // All characters in s are found in t in the same order
     }
 };
