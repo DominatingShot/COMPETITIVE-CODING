2898ab8eb9af74d493d498067085dc350ad43b69
DominatingShot <akshajvj2003@gmail.com>
2023-07-01 12:37:41 +0400

    2

---
+++ CODE-CHEF/SUBARRAYREM/SUBARRAYREM.py
@@ -0,0 +1,15 @@
+t=int(input())
+for i in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    x=[]
+    p=0
+    for i in a:
+        x.append(i)
+        if(len(x)>2):
+            
+    print(p)
+    
+        
+        
+        
