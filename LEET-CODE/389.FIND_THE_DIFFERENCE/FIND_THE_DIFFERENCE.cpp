6c4eb8a630f7308f38bba3c121e07979b9c7e1d7
DomiantingShot <akshajvj2003@gmail.com>
2023-08-13 13:16:27 +0400

    Hi

--- LEET-CODE/389.FIND_THE_DIFFERENCE/FIND_THE_DIFFERENCE.cpp
+++ LEET-CODE/389.FIND_THE_DIFFERENCE/FIND_THE_DIFFERENCE.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        map<char, int> m;
-    }
-};
+        char c=0;
+        for(char cs:s)
+            c^=cs;
+        for(char cs:t)
+            c^=cs;
+        return c;
+    };
+}
